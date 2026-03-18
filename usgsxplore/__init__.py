5728ccbe4b4a8f3446395b69aa7f429d3db26993
godinlu <godin.luc2002@gmail.com>
2026-03-18 09:08:27 +0100

    removing the vizualisation file

--- usgsxplore/__init__.py
+++ usgsxplore/__init__.py
@@ -1,12 +1,4 @@
 from usgsxplore import filter, utils, browse, core  # noqa: F401  # exposed for public API
 from usgsxplore.api import API  # noqa: F401  # exposed for public API
-from . import vizualisation as viz
 
-__all__ = [
-    "filter",
-    "utils",
-    "browse",
-    "core",
-    "API",
-    "viz",
-]
+__all__ = ["filter", "utils", "browse", "core", "API"]
